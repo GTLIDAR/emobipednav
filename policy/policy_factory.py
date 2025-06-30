e1c270494ca07da1bf77dd2a2130f51111e4b912
wei.zhu <45971057+zw199502@users.noreply.github.com>
2025-06-30 09:26:05 +0900

    Update policy_factory.py

--- policy/policy_factory.py
+++ policy/policy_factory.py
@@ -1,7 +1,7 @@
 from policy.orca import ORCA
-from policy.sfm import SFM
+# from policy.sfm import SFM
 
 policy_factory = dict()
 policy_factory['orca'] = ORCA
-policy_factory['sfm'] = SFM
+# policy_factory['sfm'] = SFM
 
